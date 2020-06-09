15b78880e0965f6aa72bc8e83f91886c46b2b1f1
itsyukai <itsyukai@gmail.com>
2020-06-09 00:58:57 -0700

    removing drop shadow

--- src/components/trianglething.js
+++ src/components/trianglething.js
@@ -9,14 +9,7 @@ const TriangleThing = () => (
     xmlns="http://www.w3.org/2000/svg"
     class="svgcolor-light"
   >
-    <path d="M0 0 L50 100 L100 0 Z" fill="white" stroke="white">
-      <defs>
-        <filter id="f1" x="0" y="0" width="200%" height="200%">
-          <feOffset result="offOut" in="SourceGraphic" dx="10" dy="10" />
-          <feBlend in="SourceGraphic" in2="offOut" mode="normal" />
-        </filter>
-      </defs>
-    </path>
+    <path d="M0 0 L50 100 L100 0 Z" fill="white" stroke="white"></path>
   </svg>
 )
 
