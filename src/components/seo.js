c46fe271a0e466c93b910d8659d00bad4994495f
itsyukai <itsyukai@gmail.com>
2020-06-09 01:00:53 -0700

    update

--- src/components/seo.js
+++ src/components/seo.js
@@ -36,6 +36,10 @@ function SEO({ description, lang, meta, title }) {
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
         {
+          name: `viewport`,
+          content: `minimum-scale=1, initial-scale=1, width=device-width`,
+        },
+        {
           name: `description`,
           content: metaDescription,
         },
