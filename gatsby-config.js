22c569571eb6bc3c22b97a7c2722e35bd364bd8c
itsyukai <itsyukai@gmail.com>
2020-06-09 00:53:32 -0700

    puts logo in the tab

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Yukai's Portfolio`,
+    title: `Yukai Yang | Software Engineer`,
     description: `Look at all this coolshit I made.`,
     author: `Yukai`,
   },
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
